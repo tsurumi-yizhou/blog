47d61b6dec14f4366948a8197dbfa0de02b84daa
yizhou <tongyz2922@mails.jlu.edu.cn>
2024-06-12 01:10:10 +0800

    refactor: move to nanostores

--- src/utils/notion.ts
+++ src/utils/notion.ts
@@ -1,34 +1,57 @@
-import { config } from "dotenv";
-import { Client } from "@notionhq/client";
 
-async function getClient() {
-  config();
-  return await new Client({
-    auth: process.env.API_SECRET
-  })
+import { Client } from "@notionhq/client";
+import type { DatabaseObjectResponse, GetDatabaseResponse, PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+
+export interface Book {
+  title: string;
+  cover: string | undefined;
+  author: string | undefined;
+  publisher: string | undefined;
+  status: string;
+  link: string;
 }
 
 export async function getBooks() {
   var has_more = true;
   var cursor: string | undefined = undefined;
-  var results = undefined;
-  const notion = await getClient();
+  var results: PageObjectResponse[] | undefined = undefined;
+
+  const notion = new Client({
+    auth: process.env.API_SECRET
+  });
+  
   while (has_more) {
+
     const database = await notion.databases.query({
       database_id: process.env.DATABASE as string,
       start_cursor: cursor,
     });
 
     if (results == undefined) {
-      results = database.results;
+      results = database.results as PageObjectResponse[];
       console.log(database.results.length);
     } else {
-      results = results.concat(database.results);
+      results = results.concat(database.results as PageObjectResponse[]);
       console.log(database.results.length);
     }
 
     cursor = database.next_cursor??undefined;
-    has_more = database.has_more;
+    has_more = false; //database.has_more;
   }
-  return results;
+
+  return results?.map(result => {
+    const title = result.properties.Name.title[0].plain_text;
+    const cover = result.cover?.external.url;
+    const author = result.properties?.author?.rich_text[0]?.plain_text;
+    const publisher = result.properties?.publisher?.rich_text[0]?.plain_text;
+    const status = result.archived;
+    return {
+      title: title,
+      cover: cover,
+      author: author,
+      publisher: publisher,
+      status: status ? "legere" : "lectio",
+      link: `/books/${title}`
+    } satisfies Book;
+  });
 }
---
+++ src/utils/state.ts
@@ -0,0 +1,5 @@
+import { atom } from "nanostores";
+
+export const currentCategory = atom("");
+export const currentBookTab = atom("legere");
+export const currentTitle = atom("");
