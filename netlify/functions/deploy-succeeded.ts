7166c08a3bc76bf55bb3d948f225f1c28ec73b3c
Don Yihtseu <i@yizhou.ac.cn>
2025-03-21 18:27:39 +0800

    feat: refresh cdn
    Signed-off-by: Don Yihtseu <i@yizhou.ac.cn>

---
+++ netlify/functions/deploy-succeeded.ts
@@ -0,0 +1,19 @@
+import {Client} from "tencentcloud-sdk-nodejs-cdn/tencentcloud/services/cdn/v20180606/cdn_client";
+import type {Context} from "@netlify/functions";
+import type {PurgePathCacheRequest} from "tencentcloud-sdk-nodejs-cdn/tencentcloud/services/cdn/v20180606/cdn_models";
+
+export default async (request: Request, context: Context) => {
+    const client = new Client({
+        credential: {
+            secretId: process.env.SECRET_ID,
+            secretKey: process.env.SECRET_KEY,
+        },
+        profile: {
+            httpProfile: {
+                endpoint: "https://blog.yizhou.ac.cn"
+            }
+        }
+    })
+    context.waitUntil(client.PurgePathCache({} as PurgePathCacheRequest));
+    return new Response("OK")
+}
